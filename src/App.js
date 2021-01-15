5dd783daeca02d5b353089a26fc019f2d2fc3391
codeminnam <code.minnam@gmail.com>
2021-01-16 01:10:22 +0900

    feat: set routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductsList from './pages/ProductsList';
+import Cart from './pages/Cart';
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/products" component={ProductsList} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/" component={ProductsList} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
