9a553b2c973f2aff49a6fdf2df9cef71c4cfdbcd
codeminnam <code.minnam@gmail.com>
2021-01-17 04:17:21 +0900

    feat: style header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import styled from 'styled-components';
 import GlobalStyle from './theme/globalStyles';
 import Layout from './components/Layout';
 import CartContainer from './containers/CartContainer';
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faShoppingCart);
+
+const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px 40px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #eaecef;
+`;
+
+const Title = styled.h1`
+  font-weight: 900;
+  line-height: 2.3;
+`;
+
+const Button = styled.button`
+  background-color: #ffffff;
+  padding: 10px;
+  border: 1px solid #f5f5f7;
+  border-radius: 50%;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #f5f5f7;
+  }
+`;
 
 const Header = () => {
-  return <div>Header Component</div>;
+  return (
+    <HeaderContainer>
+      <Title>CLASS CART</Title>
+      <Button type="button">
+        <FontAwesomeIcon icon={['fa', 'shopping-cart']} />
+      </Button>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
