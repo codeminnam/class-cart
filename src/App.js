6913c63c8f61fe7c9263eead6ac7b3bf57b82bad
codeminnam <code.minnam@gmail.com>
2021-01-17 03:18:53 +0900

    build: reset css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import GlobalStyle from './theme/globalStyles';
 import Layout from './components/Layout';
 import CartContainer from './containers/CartContainer';
 import ProductListContainer from './containers/ProductListContainer';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Layout>
-          <Route exact path="/products" component={ProductListContainer} />
-          <Route exact path="/cart" component={CartContainer} />
-          <Route exact path="/" component={ProductListContainer} />
-        </Layout>
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Layout>
+            <Route exact path="/products" component={ProductListContainer} />
+            <Route exact path="/cart" component={CartContainer} />
+            <Route exact path="/" component={ProductListContainer} />
+          </Layout>
+        </Switch>
+      </Router>
+    </>
   );
 };
 
